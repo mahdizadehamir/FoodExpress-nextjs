1556e6ef9695a80f22ef57434eb954d10ee32fdc
mahdizadehamir <mehdizadeh.amir11@gmail.com>
2024-03-03 22:30:51 +0330

    Menu Paths Configured and attached to each food Details Page

---
+++ pages/menu/[id].js
@@ -0,0 +1,44 @@
+import { useRouter } from 'next/router';
+
+function Details({ data }) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h2>Loading Page ... </h2>;
+  }
+  return <div>Details</div>;
+}
+
+export default Details;
+
+export async function getStaticPaths() {
+  const res = await fetch('http://localhost:4000/data');
+  const data = await res.json();
+  const slicedData = data.slice(0, 10);
+  const paths = slicedData.map((food) => ({
+    params: {
+      id: food.id.toString(),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const res = await fetch(`http://localhost:4000/data/${params.id}`);
+  const data = await res.json();
+
+  if (!data.name) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { data },
+    revalidate: 10, //seconds
+  };
+}
